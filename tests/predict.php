9ec48d8c68fad02b38ad752e61147fcfc3a8728f
я котик <100466152+k1ttyf@users.noreply.github.com>
2024-04-21 14:56:15 +0200

    Create predict.php

---
+++ tests/predict.php
@@ -0,0 +1,22 @@
+<?php
+
+$image = __DIR__ . "/image.jpeg";
+require_once __DIR__ . "/vendor/autoload.php";
+$GeoSpy = new k1ttyf\GeoSpy\API("YOUR_API_KEY");
+
+try {
+    $result = $GeoSpy->predict($image);
+    foreach($result["geo_predictions"] as $value){
+        echo "Latitude: " . $value["coordinates"][0] . PHP_EOL;
+        echo "Longitude: " . $value["coordinates"][1] . PHP_EOL;
+        foreach($GeoSpy->getLocationLinks($value["coordinates"][0], $value["coordinates"][1]) as $service => $link){
+            echo ucfirst($service) . ": " . $link . PHP_EOL;
+        }
+        echo "Score: " . $value["score"] . PHP_EOL;
+        echo "Similarity Score (1km): " . $value["similarity_score_1km"] . PHP_EOL;
+        echo "Address: " . $value["address"] . PHP_EOL;
+        echo PHP_EOL;
+    }
+} catch(Exception $e){
+    echo "Error: " . $e->getMessage() . PHP_EOL;
+}
